92127eb7defe8f18fb2ca81a27e597221b2f209d
jt0109 <john0109tsai@gmail.com>
2015-11-29 21:15:50 +0800

    Put an icon on the map after map init.

--- app/src/main/java/org/csie/cheertour/cheertour/Map/TravelMapFragment.java
+++ app/src/main/java/org/csie/cheertour/cheertour/Map/TravelMapFragment.java
@@ -1,14 +1,27 @@
 package org.csie.cheertour.cheertour.Map;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.ClusterItem;
+import com.google.maps.android.ui.IconGenerator;
 
 import org.csie.cheertour.cheertour.R;
 
@@ -16,10 +29,11 @@ import org.csie.cheertour.cheertour.R;
 /**
  * Created by rose-pro on 2015/7/12.
  */
-public class TravelMapFragment extends Fragment {
+public class TravelMapFragment extends Fragment{
     private View rootView;
     private MapView mapView;
     private GoogleMap map;
+    private UiSettings uiSettings;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if(container==null)
@@ -37,6 +51,23 @@ public class TravelMapFragment extends Fragment {
         }
 
         map = mapView.getMap();
+
+        // Show Zoom control
+        uiSettings = map.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+
+        // Add a marker and set map center to its position
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(25.02, 121.38), 15));
+
+        IconGenerator iconGenerator = new IconGenerator( this.getActivity() );
+        Bitmap iconBitmap = iconGenerator.makeIcon("Cheertour");
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromBitmap(iconBitmap))
+                .position(new LatLng(25.02, 121.38))
+                .title("visit cheertour.info now")
+        );
+        // map.setOnMarkerClickListener();
+
         return rootView;
     }
 
